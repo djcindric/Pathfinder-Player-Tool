6fe85c956f6374e8290a0db0f780275788e30149
msjohns9 <msjohns9@uncg.edu>
2014-09-13 10:50:50 -0400

    Added some fields

--- PathfinderPlayerTool/src/com/example/pathfinderplayertool/Ability.java
+++ PathfinderPlayerTool/src/com/example/pathfinderplayertool/Ability.java
@@ -1,5 +1,20 @@
 package com.example.pathfinderplayertool;
 
 public class Ability {
-
+	//Name of the ability
+	private String name;
+	
+	//Description of what the ability does
+	private String descr;
+	
+	//Type of ability that it is. Ex = Extraordinary. Sp = Spell-like. Su = Supernatural
+	private String type;
+	
+	/*
+	 * Some abilities provide bonuses such as an increased attribute, damage reduction,
+	 * or a spell. We will need a way to apply these bonuses to the character sheet if
+	 * this ability is somehow added.
+	 * 
+	 * private Bonus bonuses;
+	 */
 }
